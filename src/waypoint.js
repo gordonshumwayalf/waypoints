9e03108c7d7f841b2077d5196bef397b860f1995
gordonshumwayalf <nikolaichernichenko@gmail.com>
2025-02-19 16:14:59 +0200

    Update waypoint.js

--- src/waypoint.js
+++ src/waypoint.js
@@ -1,146 +1,100 @@
-(function() {
-  'use strict'
+(function () {
+  'use strict';
 
-  var keyCounter = 0
-  var allWaypoints = {}
+  let keyCounter = 0;
+  const allWaypoints = new Map();
 
-  /* http://imakewebthings.com/waypoints/api/waypoint */
-  function Waypoint(options) {
-    if (!options) {
-      throw new Error('No options passed to Waypoint constructor')
+  class Waypoint {
+    constructor(options) {
+      if (!options?.element) throw new Error('No element option passed to Waypoint constructor');
+      if (!options?.handler) throw new Error('No handler option passed to Waypoint constructor');
+
+      this.key = `waypoint-${keyCounter++}`;
+      this.options = Object.assign({}, Waypoint.defaults, options);
+      this.element = this.options.element;
+      this.adapter = new Waypoint.Adapter(this.element);
+      this.callback = options.handler;
+      this.axis = this.options.horizontal ? 'horizontal' : 'vertical';
+      this.enabled = this.options.enabled;
+      this.triggerPoint = null;
+      this.group = Waypoint.Group.findOrCreate({ name: this.options.group, axis: this.axis });
+      this.context = Waypoint.Context.findOrCreateByElement(this.options.context);
+
+      if (Waypoint.offsetAliases[this.options.offset]) {
+        this.options.offset = Waypoint.offsetAliases[this.options.offset].call(this);
+      }
+
+      this.group.add(this);
+      this.context.add(this);
+      allWaypoints.set(this.key, this);
     }
-    if (!options.element) {
-      throw new Error('No element option passed to Waypoint constructor')
-    }
-    if (!options.handler) {
-      throw new Error('No handler option passed to Waypoint constructor')
+
+    queueTrigger(direction) {
+      this.group.queueTrigger(this, direction);
     }
 
-    this.key = 'waypoint-' + keyCounter
-    this.options = Waypoint.Adapter.extend({}, Waypoint.defaults, options)
-    this.element = this.options.element
-    this.adapter = new Waypoint.Adapter(this.element)
-    this.callback = options.handler
-    this.axis = this.options.horizontal ? 'horizontal' : 'vertical'
-    this.enabled = this.options.enabled
-    this.triggerPoint = null
-    this.group = Waypoint.Group.findOrCreate({
-      name: this.options.group,
-      axis: this.axis
-    })
-    this.context = Waypoint.Context.findOrCreateByElement(this.options.context)
-
-    if (Waypoint.offsetAliases[this.options.offset]) {
-      this.options.offset = Waypoint.offsetAliases[this.options.offset]
+    trigger(args) {
+      if (this.enabled && this.callback) {
+        this.callback(...args);
+      }
     }
-    this.group.add(this)
-    this.context.add(this)
-    allWaypoints[this.key] = this
-    keyCounter += 1
-  }
 
-  /* Private */
-  Waypoint.prototype.queueTrigger = function(direction) {
-    this.group.queueTrigger(this, direction)
-  }
-
-  /* Private */
-  Waypoint.prototype.trigger = function(args) {
-    if (!this.enabled) {
-      return
+    destroy() {
+      this.context.remove(this);
+      this.group.remove(this);
+      allWaypoints.delete(this.key);
     }
-    if (this.callback) {
-      this.callback.apply(this, args)
+
+    disable() {
+      this.enabled = false;
+      return this;
     }
-  }
 
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/destroy */
-  Waypoint.prototype.destroy = function() {
-    this.context.remove(this)
-    this.group.remove(this)
-    delete allWaypoints[this.key]
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/disable */
-  Waypoint.prototype.disable = function() {
-    this.enabled = false
-    return this
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/enable */
-  Waypoint.prototype.enable = function() {
-    this.context.refresh()
-    this.enabled = true
-    return this
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/next */
-  Waypoint.prototype.next = function() {
-    return this.group.next(this)
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/previous */
-  Waypoint.prototype.previous = function() {
-    return this.group.previous(this)
-  }
-
-  /* Private */
-  Waypoint.invokeAll = function(method) {
-    var allWaypointsArray = []
-    for (var waypointKey in allWaypoints) {
-      allWaypointsArray.push(allWaypoints[waypointKey])
+    enable() {
+      this.context.refresh();
+      this.enabled = true;
+      return this;
     }
-    for (var i = 0, end = allWaypointsArray.length; i < end; i++) {
-      allWaypointsArray[i][method]()
+
+    next() {
+      return this.group.next(this);
+    }
+
+    previous() {
+      return this.group.previous(this);
+    }
+
+    static invokeAll(method) {
+      allWaypoints.forEach(waypoint => waypoint[method]());
+    }
+
+    static destroyAll() {
+      Waypoint.invokeAll('destroy');
+    }
+
+    static disableAll() {
+      Waypoint.invokeAll('disable');
+    }
+
+    static enableAll() {
+      Waypoint.Context.refreshAll();
+      allWaypoints.forEach(waypoint => (waypoint.enabled = true));
+    }
+
+    static refreshAll() {
+      Waypoint.Context.refreshAll();
+    }
+
+    static viewportHeight() {
+      return window.innerHeight || document.documentElement.clientHeight;
+    }
+
+    static viewportWidth() {
+      return document.documentElement.clientWidth;
     }
   }
 
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/destroy-all */
-  Waypoint.destroyAll = function() {
-    Waypoint.invokeAll('destroy')
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/disable-all */
-  Waypoint.disableAll = function() {
-    Waypoint.invokeAll('disable')
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/enable-all */
-  Waypoint.enableAll = function() {
-    Waypoint.Context.refreshAll()
-    for (var waypointKey in allWaypoints) {
-      allWaypoints[waypointKey].enabled = true
-    }
-    return this
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/refresh-all */
-  Waypoint.refreshAll = function() {
-    Waypoint.Context.refreshAll()
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/viewport-height */
-  Waypoint.viewportHeight = function() {
-    return window.innerHeight || document.documentElement.clientHeight
-  }
-
-  /* Public */
-  /* http://imakewebthings.com/waypoints/api/viewport-width */
-  Waypoint.viewportWidth = function() {
-    return document.documentElement.clientWidth
-  }
-
-  Waypoint.adapters = []
+  Waypoint.adapters = [];
 
   Waypoint.defaults = {
     context: window,
@@ -148,17 +102,17 @@
     enabled: true,
     group: 'default',
     horizontal: false,
-    offset: 0
-  }
+    offset: 0,
+  };
 
   Waypoint.offsetAliases = {
-    'bottom-in-view': function() {
-      return this.context.innerHeight() - this.adapter.outerHeight()
+    'bottom-in-view': function () {
+      return this.context.innerHeight() - this.adapter.outerHeight();
     },
-    'right-in-view': function() {
-      return this.context.innerWidth() - this.adapter.outerWidth()
-    }
-  }
+    'right-in-view': function () {
+      return this.context.innerWidth() - this.adapter.outerWidth();
+    },
+  };
 
-  window.Waypoint = Waypoint
-}())
+  window.Waypoint = Waypoint;
+})();
